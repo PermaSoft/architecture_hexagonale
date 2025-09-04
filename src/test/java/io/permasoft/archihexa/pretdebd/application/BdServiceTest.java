5c832f8d0f72462cb100f66f6d191591c94cd57d
Nicolas <nicolas.fedou@gmail.com>
2025-09-04 10:46:00 +0200

    Make an application test to define expectations of the use case. Each compilation error shall imply creating empty production code. Empty production code is in next commit.

---
+++ src/test/java/io/permasoft/archihexa/pretdebd/application/BdServiceTest.java
@@ -0,0 +1,47 @@
+package io.permasoft.archihexa.pretdebd.application;
+
+import io.permasoft.archihexa.pretdebd.domain.Bd;
+import io.permasoft.archihexa.pretdebd.domain.Bds;
+import io.permasoft.archihexa.pretdebd.domain.Isbn;
+import io.permasoft.archihexa.pretdebd.domain.Proprietaire;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BdServiceTest {
+    // each test is run after creating a new instance of the test class, so instance variables are new for each tests.
+    Repository repo = new Repository() {
+        Bds bds = new Bds(List.of());
+
+        @Override
+        public void save(Bd bd) {
+            bds = bds.add(bd);
+        }
+
+        @Override
+        public Bd load(UUID id) {
+            return bds.byId(id);
+        }
+    };
+
+    @Test
+    void enregitre_une_bd_et_la_consulte() {
+        // GIVEN
+        UUID id = UUID.randomUUID();
+        Proprietaire proprietaire = new Proprietaire("Quentin");
+        Isbn isbn = new Isbn("978-2012101333");
+        //WHEN
+        BdService bdService = new BdService(repo);
+        bdService.enregistrer(id, isbn, proprietaire);
+        //THEN
+        Bd bd = bdService.byId(id);
+        assertThat(new Bd(isbn, proprietaire, Bd.State.DISPONIBLE))
+                .usingRecursiveComparison()
+                .isEqualTo(bd);
+
+    }
+}
